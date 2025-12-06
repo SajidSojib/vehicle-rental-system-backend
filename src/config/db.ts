b91f9d4a834047354ffb52bc134bbd9cf67f4df8
SajidSojib <sajidahmed2401@gmail.com>
2025-12-06 14:53:57 +0600

    vehicle db created

--- src/config/db.ts
+++ src/config/db.ts
@@ -17,6 +17,16 @@ const initDB = async () => {
             phone VARCHAR(15) NOT NULL,
             role VARCHAR(15) NOT NULL CHECK (role IN ('admin', 'customer'))
         );`);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles (
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(255) NOT NULL,
+            type VARCHAR(5) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+            registration_number VARCHAR(255) NOT NULL UNIQUE,
+            daily_rent_price INT NOT NULL CHECK (daily_rent_price > 0),
+            availability_status VARCHAR(15) DEFAULT 'available' CHECK (availability_status IN ('available', 'booked'))
+        )`);
     
     console.log('db connected');
 };
